eab9dc7332737f1460a166056cae89894d935fa3
Jack <1162817095@qq.com>
2017-05-08 23:11:16 +0800

    code in 5-8

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -2,8 +2,11 @@
 
 	var app = this.nb;
 
-	var homeController = function($scope, $rootScope, $http) {
-
+	var homeController = function($scope, $rootScope, $http, $timeout) {
+		$scope.isFade = false;
+		$timeout(function() {
+			$scope.isFade = true;
+		}, 1000)
 	}
 
 	app.controller('homeController', homeController);
--- public/scripts/controllers/regController.js
+++ public/scripts/controllers/regController.js
@@ -8,6 +8,10 @@
 			{'name': 'male', 'text': '男'}, 
 			{'name': 'female', 'text': '女'}
 		];
+
+		this.reg = function() {
+			console.log($scope.reg)
+		}
 	}
 
 	app.controller('regController', regController);
