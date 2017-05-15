52d115fe905c70c52b9aac04eaa0bd158ee8b666
Jack <1162817095@qq.com>
2017-05-15 20:23:44 +0800

    code

--- public/scripts/services/homeService.js
+++ public/scripts/services/homeService.js
@@ -3,16 +3,18 @@
 	var app = this.nb;
 
 	app.factory("homeService", function() {
-		var homeDirectiveData = [
-			{
-				src: 'assets/images/home/wife1.jpg',
-				desc: 'wife1'
-			},
-			{
-				src: 'assets/images/home/wife2.jpg',
-				desc: 'wife2'
+		var srcStr = 'assets/images/home/wife', descStr = 'wife';
+		var homeDirectiveData = [];
+		for(var i = 0; i < 50; i++) {
+			var num = i % 7 + 1;
+			var obj = {
+				src: srcStr + num + '.jpg',
+				desc: descStr +  num
 			}
-		]
+			homeDirectiveData.push(obj);
+		}
+
+		console.log(homeDirectiveData);
 
 
 
